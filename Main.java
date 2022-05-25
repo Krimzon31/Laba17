a303418535dba544243268f6dfa068cfb0c8e72b
Krimzon31 <83700861+Krimzon31@users.noreply.github.com>
2022-05-25 16:20:18 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,56 @@
+package com.company;
+
+abstract class Transportation{
+    String name;
+    public String toString(){
+        return"View Transportation: " + name;
+    }
+}
+abstract  class TransportationFatory{
+    public abstract Transportation createTransport();
+}
+class TruckFactory extends TransportationFatory{
+    public Transportation createTransport(){
+        return new Truck();
+    }
+}
+class AirplaneFactory extends TransportationFatory{
+    public Transportation createTransport(){
+        return new Airplane();
+    }
+}
+class Truck extends Transportation{
+    public Truck(){
+        name = "Delivery by truck";
+    }
+}
+class Airplane extends Transportation{
+    public Airplane(){
+        name = "Delivery by sky";
+    }
+}
+class ShipFactory extends TransportationFatory{
+    public Transportation createTransport(){
+        return new Ship();
+    }
+}
+class Ship extends Transportation{
+    public Ship(){
+        name = "Shipping by sea";
+    }
+}
+public class Main {
+
+    public static void main(String[] args) {
+        TransportationFatory truckDelivery = new TruckFactory();
+        TransportationFatory airplaneDelivery = new AirplaneFactory();
+        Transportation truckDel = truckDelivery.createTransport();
+        Transportation airDel = airplaneDelivery.createTransport();
+        TransportationFatory oceanDelivery = new TruckFactory();
+        Transportation oceanDel = truckDelivery.createTransport();
+        System.out.println(truckDel);
+        System.out.println(airDel);
+        System.out.println(oceanDel);
+
+    }
+}
